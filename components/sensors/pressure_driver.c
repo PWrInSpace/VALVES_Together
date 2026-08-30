1c6ff9afaffb025bed090e682e35e7147f47d486
Bart3kTK <github-actions@github.com>
2026-08-30 20:29:03 +0200

    Pressure driver improvment

--- components/sensors/pressure_driver.c
+++ components/sensors/pressure_driver.c
@@ -16,7 +16,6 @@
 #include <stddef.h>
 
 #define TAG "PRESSURE_DRIVER"
-#define PRESSURE_DRIVER_MAX_SAMPLES 5
 
 static ads1115_struct_t ads1115_config = {
     ._i2c_write = _mcu_i2c_write,
@@ -87,15 +86,16 @@ pressure_driver_set_1_pressure(pressure_driver_struct_t *pressure_driver,
 pressure_driver_status_t
 pressure_driver_read_voltage(pressure_driver_struct_t *pressure_driver,
                              pressure_driver_sensor_t sensor, float *voltage) {
-  if (pressure_driver == NULL)
-    return PRESSURE_DRIVER_FAIL;
 
+  if (pressure_driver == NULL || voltage == NULL)
+    return PRESSURE_DRIVER_FAIL;
   int16_t raw;
 
-  ads1115_get_value(pressure_driver->ads1115, &raw);
   ads1115_set_input_mux(pressure_driver->ads1115,
                         pressure_driver->sensors[sensor].adc_pin);
-  esp_rom_delay_us(4000);
+  esp_rom_delay_us(
+      2400); // one conversion time is 1/860 seconds (1.16 ms) after mux change
+             // in worst case ads1115 takes time of two conversions (2.32 ms)
   ads1115_get_value(pressure_driver->ads1115, &raw);
   *voltage = ads1115_gain_values[ADS1115_GAIN_4V096] / ADS1115_MAX_VALUE * raw;
 
@@ -127,13 +127,12 @@ pressure_driver_read_pressures(pressure_driver_struct_t *pressure_driver,
     return PRESSURE_DRIVER_FAIL;
   }
 
-  // for (int i = 0; i < PRESSURE_DRIVER_SENSOR_COUNT; i++) { //press 0 -
-  // termistor jest unused
+  // termistor is not used so loop starts from 1 (sensor 0 is not used)
   for (int i = 1; i < PRESSURE_DRIVER_SENSOR_COUNT; i++) {
     // Read voltage for the current sensor
     float v_raw;
     pressure_driver_status_t status =
-        measure_voltage_mean_from_samples(pressure_driver, i, &v_raw);
+        pressure_driver_read_voltage(pressure_driver, i, &v_raw);
     if (status != PRESSURE_DRIVER_OK) {
       ESP_LOGE(TAG, "Failed to read voltage for sensor %d", i);
       return status;
@@ -155,46 +154,6 @@ pressure_driver_read_pressures(pressure_driver_struct_t *pressure_driver,
 }
 
 pressure_driver_status_t
-measure_voltage_mean_from_samples(pressure_driver_struct_t *pressure_driver,
-                                  pressure_driver_sensor_t sensor,
-                                  float *out_voltage) {
-  if (pressure_driver == NULL || out_voltage == NULL) {
-    ESP_LOGE(TAG, "Invalid argument in measure_voltage_mean_from_samples");
-    return PRESSURE_DRIVER_FAIL;
-  }
-
-  float voltage = 0.0f;
-
-  float voltage_samples[PRESSURE_DRIVER_MAX_SAMPLES];
-  size_t samples = PRESSURE_DRIVER_MAX_SAMPLES;
-  for (int i = 0; i < samples; i++) {
-    pressure_driver_status_t ret =
-        pressure_driver_read_voltage(pressure_driver, sensor, &voltage);
-    if (ret != PRESSURE_DRIVER_OK) {
-      return ret;
-    }
-    voltage_samples[i] = voltage;
-  }
-
-  for (size_t i = 1; i < samples; i++) {
-    float key = voltage_samples[i];
-    size_t j = i;
-    while (j > 0 && voltage_samples[j - 1] > key) {
-      voltage_samples[j] = voltage_samples[j - 1];
-      j--;
-    }
-    voltage_samples[j] = key;
-  }
-
-  *out_voltage =
-      (samples % 2 == 0)
-          ? (voltage_samples[samples / 2 - 1] + voltage_samples[samples / 2]) /
-                2.0f
-          : voltage_samples[samples / 2];
-  return PRESSURE_DRIVER_OK;
-}
-
-pressure_driver_status_t
 calibrate_pressure_sensor(pressure_driver_struct_t *pressure_driver,
                           pressure_driver_sensor_t sensor, float pressure,
                           float *measured_volt) {
@@ -203,9 +162,9 @@ calibrate_pressure_sensor(pressure_driver_struct_t *pressure_driver,
     return PRESSURE_DRIVER_FAIL;
   }
 
-  if (measure_voltage_mean_from_samples(pressure_driver, sensor,
+  if (pressure_driver_read_voltage(pressure_driver, sensor,
 
-                                        measured_volt) != PRESSURE_DRIVER_OK) {
+                                   measured_volt) != PRESSURE_DRIVER_OK) {
     return PRESSURE_DRIVER_FAIL;
   }
 
@@ -224,9 +183,9 @@ tare_pressure_sensor(pressure_driver_struct_t *pressure_driver,
     return PRESSURE_DRIVER_FAIL;
   }
 
-  if (measure_voltage_mean_from_samples(pressure_driver, sensor,
+  if (pressure_driver_read_voltage(pressure_driver, sensor,
 
-                                        measured_volt) != PRESSURE_DRIVER_OK) {
+                                   measured_volt) != PRESSURE_DRIVER_OK) {
     return PRESSURE_DRIVER_FAIL;
   }
 
